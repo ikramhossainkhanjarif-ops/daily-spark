3a4f70d7621f219c2a8fd95239a294e3eeeeadb7
Ikram Hossain Khan Jarif <ikramhossainkhanjarif@gmail.com>
2026-07-19 22:56:31 +0600

    Add PendingIntent import to AlarmRingingService

--- android/app/src/main/kotlin/com/dailyspark/app/AlarmRingingService.kt
+++ android/app/src/main/kotlin/com/dailyspark/app/AlarmRingingService.kt
@@ -3,6 +3,7 @@ package com.dailyspark.app
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.media.AudioAttributes
